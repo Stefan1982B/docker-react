a3b1938d934fe1a4688566cc0c8840a847ccaf81
Stefan Schuddinck <stefanschuddinck@Stefans-MacBook-Pro.local>
2023-09-15 09:57:18 +0200

    add tet

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hi there en nog iets erbij vanuit ergens
-          anders.
+          Edit <code>src/App.js</code> hi there, i was changed on the feature
+          branch.
         </p>
         <a
           className="App-link"
